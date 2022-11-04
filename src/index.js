407c8f7d4a8ae6b0878aea097381a19b052b89d9
Jakob <45669207+gue-ni@users.noreply.github.com>
2022-11-04 12:35:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/sw.js").then(function () {
+	navigator.serviceWorker.register("/project/corona/sw.js").then(function () {
 		console.log("Service Worker Registered");
 	});
 } else {
